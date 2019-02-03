e1574d08761ecafb07b7b734fba7db55ed8d1269
K L Naga kishore <lakshminagakishore@github.com>
2019-02-03 13:15:36 +0530

    test message

--- Project_Backend/src/main/java/com/app/controller/ProjectCntroller.java
+++ Project_Backend/src/main/java/com/app/controller/ProjectCntroller.java
@@ -1,7 +1,5 @@
 package com.app.controller;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -10,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
